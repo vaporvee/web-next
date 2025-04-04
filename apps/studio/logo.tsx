7864d959f7fde44be2dc9c40d0759bad735bc140
vaporvee <yannik@vaporvee.com>
2025-04-04 22:11:16 +0200

    rewrite nr 5000

--- apps/studio/logo.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const logo = () => <img src="/static/favicon.svg" alt="Favicon" />
-
-export default logo
--- apps/web/src/components/client-providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { NextUIProvider } from "@nextui-org/react";
-import { ReactNode } from "react";
-
-export default function ClientProviders({ children }: { children: ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
-}
