7229747627526e50fcebc161c3ec6d7db8780e9b
vaporvee <yannik@vaporvee.com>
2025-04-07 23:20:23 +0200

    colors

--- apps/personal/src/app/layout.tsx
+++ apps/personal/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} dark antialiased scroll-smooth`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}
       >
         <Navbar />
         {children}
